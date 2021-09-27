4987f42b3a3bcaf8e8b153336009e7814505e333
Jamie <jamiespekman@hotmail.com>
2021-09-27 13:30:18 +0200

    Initial commit

---
+++ src/test/java/nl/jamienovi/garagemanagement/GaragemanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.jamienovi.garagemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaragemanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
